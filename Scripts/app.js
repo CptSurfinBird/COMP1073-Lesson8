62b0d4e7763278b02dfd2c34aad5dbd84edbbcea
CptSurfinBird <jacobgittings@gmail.com>
2016-03-10 09:58:26 -0500

    use strict moved in app.js

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,28 +1,8 @@
-"use strict";
-
 // setup your IIFE (Immediately Invoked Function Expression)
 (function () {
 
-	var checkMeOut;
+|"use strict";
 
-	checkMeOut = document.getElementById("checkMeOut");
-	console.log(checkMeOut);
 
-	/*
-	checkMeOut.onclick(function(){
-		console.log("checkMeOut clicked");
-	});
-	*/
-
-	checkMeOut.addEventListener("change", function () {
-		/*
-		if (checkMeOut.checked) {
-			console.log("It's checked");
-		} else {
-			console.log("It's unchecked");
-		}*/
-		
-		(checkMeOut.checked)? console.log("It's checked") : console.log("It's unchecked"); 
-	});
 
 })();
